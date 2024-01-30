97e467f110382e78a9cef456c03b9d203ea47e0b
swierk434 <swierkpl@gmail.com>
2024-01-31 00:36:08 +0100

    commit

--- src/Madelbrot/Mandelbrot.java
+++ src/Madelbrot/Mandelbrot.java
@@ -10,22 +10,25 @@ public class Mandelbrot {
 		Image I[] = new Image[sizes.length];
 		//double x1 = -0.742643135; double y1 = 0.131825963;	
 		//Image I = new Image(sizex, sizey, -2.1, 0.6, -1.2, 1.2, 200);
+		System.out.print('[');
 		for(int n = 0; n < sizes.length; n++) {
 			//I[n] = new Image(sizes[n], sizes[n], x1-0.002, x1+0.0005, y1-0.002, y1+0.0005 ,200);
 			I[n] = new Image(sizes[n], sizes[n], -2.1, 0.6, -1.2, 1.2, 200);
-			long start = System.nanoTime();
+			System.out.print('[');
 			for(int m = 0; m < 10; m++){
+				long start = System.nanoTime();
 				for(int x = 0; x < sizes[n]; x++) {
 					//System.out.println(x);
 					for(int y = 0; y < sizes[n]; y++) {
 						I[n].colorPixel(x,y);
 					}
 				}
-			
+			long end = System.nanoTime();
+			System.out.print((end - start) / 1000000000.0 + ", ");
 			ImageIO.write(I[n], "png", new File("Mandelbrot" + sizes[n] + "px.png"));
 			}
-			long end = System.nanoTime();
-			System.out.println((end - start) / 1000000000.0);
+			System.out.println(']');
 		}
+		System.out.println(']');
 	}
 }
